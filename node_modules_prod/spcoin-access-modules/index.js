c391df059a910b9cfb272caf6ba5d7f856538a5b
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-12-10 23:35:31 -0500

    WIP

--- node_modules_prod/spcoin-access-modules/index.js
+++ node_modules_prod/spcoin-access-modules/index.js
@@ -1,26 +1,20 @@
-const { SpCoinLogger, LOG_MODE } = require("./utils/logging");
+const { SpCoinLogger } = require("./utils/logging");
 const { SpCoinERC20Module } = require("./modules/spCoinERC20Module");
 const { SpCoinDeleteModule } = require("./modules/spCoinDeleteModule");
 const { SpCoinAddModule } = require("./modules/spCoinAddModule");
 const { SpCoinReadModule } = require("./modules/spCoinReadModule");
 const { SpCoinRewardsModule } = require("./modules/spCoinRewardsModule"); 
 const { SpCoinStakingModule } = require("./modules/spCoinStakingModule"); 
-const { second, minute, hour, day, week, year, month , millennium } = require("./modules/spCoinStakingModule");
 const ethers = require('ethers');
 
 class SpCoinAccessModules {
-  // constructor(_spCoinContractDeployed) {
     constructor(spCoinABI, spCoinAddress, signer) {
-    // console.log(`AAA spCoinAddress = ${spCoinAddress}`)
-    // console.log(`AAA spCoinABI = ${JSON.stringify(spCoinABI,null,2)}`)
-    // console.log(`AAA signer = ${JSON.stringify(signer,null,2)}`)
+    // console.debug(`SpCoinAccessModules.constructor.spCoinAddress = ${spCoinAddress}`)
+    // console.debug(`SpCoinAccessModules.constructor.spCoinABI = ${JSON.stringify(spCoinABI,null,2)}`)
+    // console.debug(`SpCoinAccessModules.constructor.signer = ${JSON.stringify(signer,null,2)}`)
 
-    // console.log(`AAA ethers = ${JSON.stringify(ethers,null,2)}`)
     const signedContract = new ethers.Contract(spCoinAddress, spCoinABI, signer);
 
-    // console.log(`spCoinAddress = ${spCoinAddress}`)
-    // console.log(`spCoinABI = ${JSON.stringify(spCoinABI,null,2)}`)
-    // console.log(`signer = ${JSON.stringify(signer,null,2)}`)
     this.spCoinContractDeployed = signedContract;
     this.spCoinAddMethods = new SpCoinAddModule(this.spCoinContractDeployed, signedContract);
     this.spCoinDeleteMethods = new SpCoinDeleteModule(this.spCoinContractDeployed);
