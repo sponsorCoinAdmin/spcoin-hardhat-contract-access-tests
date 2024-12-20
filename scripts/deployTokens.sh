1d0bd7d8a9affa81dcde816bc8d99b3cc9f1327f
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-12-20 02:45:15 -0500

    WIP

--- scripts/deployTokens.sh
+++ scripts/deployTokens.sh
@@ -1,4 +1,6 @@
-. $HH_SCRIPTS/deployments/deploySPCoin.sh
-. $HH_SCRIPTS/deployments/deployweth.sh
+. $HH_SCRIPTS/deployments/deployApollo.sh
 . $HH_SCRIPTS/deployments/deployChickenCoin.sh
+. $HH_SCRIPTS/deployments/deploySPCoin.sh
 . $HH_SCRIPTS/deployments/deployUSDC.sh
+. $HH_SCRIPTS/deployments/deployUSDT.sh
+. $HH_SCRIPTS/deployments/deployweth.sh
